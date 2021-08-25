8d2f24857f72f45a298b60f6b6a9a676ac4aa2fc
NikitaSharapov <nikitasharapov147@gmail.com>
2021-08-25 15:36:44 +0500

    changes

--- src/auth/auth.repository.ts
+++ src/auth/auth.repository.ts
@@ -3,7 +3,6 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from 'src/schemas/user.schema';
 import { ILoginData, IUser } from './types/auth.types';
-import { Id } from './types/common.types';
 import * as bcrypt from 'bcrypt';
 
 @Injectable()
